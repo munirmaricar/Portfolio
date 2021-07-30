316e49bad89882a235d26dfafda580ec262785d0
Munirudeen Maricar <mxm1287@cs.bham.ac.uk>
2021-07-30 19:02:39 +0100

    Fully Responsive

--- src/components/SingleExperience.js
+++ src/components/SingleExperience.js
@@ -35,17 +35,11 @@ export default function SingleExperience() {
 	if (!singleExperience) return <h1 className="text-white-100">Loading!</h1>;
 
 	return (
-		<main className="min-h-screen my-14">
-			<article className="container shadow-lg mx-auto bg-white-500 rounded-lg">
-				<header className="relative">
-					<div className="absolute h-full w-full flex items-center justify-center p-8">
-						<div className="bg-white bg-opacity-75 rounded p-12">
-							<h1 className="main-font text-3xl lg:text-6xl mb-4">
-								{singleExperience.jobTitle}
-							</h1>
-						</div>
-					</div>
-				</header>
+		<main className="px-5 align-middle mb-10 sm:mt-2 md:mt-8 lg:mt-18">
+			<h1 className="text-5xl flex text-center justify-center underline main-font page-header font-bold">
+				{singleExperience.jobTitle} at {singleExperience.company}
+			</h1>
+			{/* <article className="container mt-10 shadow-lg mx-auto rounded-lg">
 				<div className="px-16 lg:px-48 py-20 lg:py-40 prose lg:prose-xl max-w-full">
 					<BlockContent
 						className="main-font"
@@ -53,6 +47,40 @@ export default function SingleExperience() {
 						projectId="0qak8qh4"
 						dataset="production"
 					/>
+				</div>
+			</article> */}
+			<article className="relative my-10 lg:my-18 md:mx-16 md:mb-80 lg:mb-48 lg:mx-40 rounded-lg shadow-xl card-body p-16">
+				<div className="main-font text-base">
+					<span>
+						<strong className="font-bold">Location</strong>:{' '}
+						{singleExperience.location}
+					</span>
+					<br></br>
+					<span>
+						<strong className="font-bold">Started on</strong>:{' '}
+						{new Date(
+							singleExperience.startDate
+						).toLocaleDateString('en-GB')}
+					</span>
+					<br></br>
+					<span>
+						<strong className="font-bold">Finished on</strong>:{' '}
+						{new Date(singleExperience.endDate).toLocaleDateString(
+							'en-GB'
+						)}
+					</span>
+					<br></br>
+					<span>
+						<strong className="font-bold">Employment Type</strong>:{' '}
+						{singleExperience.employmentType}
+					</span>
+					<p className="mt-6 text-2xl main-font leading-relaxed">
+						<BlockContent
+							blocks={singleExperience.description}
+							projectId="0qak8qh4"
+							dataset="production"
+						/>
+					</p>
 				</div>
 			</article>
 		</main>
