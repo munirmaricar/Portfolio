1839f905e010b6ed57277b300edfd07c75f7ea2d
Munirudeen Maricar <mxm1287@cs.bham.ac.uk>
2021-07-30 11:34:52 +0100

    Making Project.js Responsive

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import sanityClient from '../client.js';
 import BlockContent from '@sanity/block-content-to-react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 
 export default function Project() {
 	const [projectData, setProjectData] = useState(null);
@@ -21,17 +23,18 @@ export default function Project() {
 	}, []);
 
 	return (
-		<main className="min-h-screen py-2">
+		<main className="px-5 align-middle sm:mt-2 mb-10 md:mt-8 lg:mt-18">
 			<section className="container mx-auto">
-				<h1 className="text-5xl flex justify-center underline main-font page-header font-bold">
+				<h1 className="text-5xl flex text-center justify-center underline main-font page-header font-bold">
 					My Projects
 				</h1>
-				<section className="py-4 grid grid-cols-1 lg:grid-cols-2 gap-8">
+				<section className="py-4 grid grid-cols-1 lg:grid-cols-2 gap-8 mt-8 lg:mt-4">
 					{projectData &&
 						projectData.map((project, index) => (
-							<article className="relative rounded-lg shadow-xl bg-white p-16">
-								<h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-red-700">
+							<article className="relative rounded-lg shadow-xl card-body p-16">
+								<h3 className="text-3xl font-bold mb-2">
 									<a
+										className="nav-item no-underline"
 										href={project.projectUrl}
 										alt={project.projectName}
 										target="_blank"
@@ -40,7 +43,17 @@ export default function Project() {
 										{project.projectName}
 									</a>
 								</h3>
-								<div className="text-gray-500 text-xs space-x-4">
+								<div className="main-font text-xs">
+									<span>
+										<strong className="font-bold">
+											Started on
+										</strong>
+										:{' '}
+										{new Date(
+											project.startDate
+										).toLocaleDateString()}
+									</span>
+									<br></br>
 									<span>
 										<strong className="font-bold">
 											Finished on
@@ -50,7 +63,7 @@ export default function Project() {
 											project.endDate
 										).toLocaleDateString()}
 									</span>
-									<p className="my-6 text-lg text-gray-700 leading-relaxed">
+									<p className="my-6 text-lg main-font leading-relaxed">
 										<BlockContent
 											blocks={project.description}
 											projectId="0qak8qh4"
@@ -61,15 +74,11 @@ export default function Project() {
 										href={project.projectUrl}
 										target="_blank"
 										rel="noopener noreferrer"
-										className="text-red-500 font-bold hover:underline hover:text-red-400"
 									>
-										View The Project{' '}
-										<span
-											role="img"
-											aria-label="right pointer"
-										>
-											👉
-										</span>
+										<FontAwesomeIcon
+											className="text-xl nav-item"
+											icon={faExternalLinkAlt}
+										/>
 									</a>
 								</div>
 							</article>
